f5c3f8683dd2eba4f77e9e1757b3907b8fce62af
haris316 <gogeto931@gmail.com>
2024-10-05 18:42:45 +0500

    Backend Initial

---
+++ app/api/team/route.js
@@ -0,0 +1,46 @@
+import { Team } from "@/lib/models";
+import { connectToDb } from "@/lib/utils";
+import { NextResponse } from "next/server";
+
+export const GET = async (req, res) => {
+  try {
+    connectToDb();
+    const teams = await Team.find();
+    return NextResponse.json({ error: false, data: teams });
+  }
+  catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      error: true,
+      err: err,
+      message: "An unexpected error occurred please try again later"
+    });
+  }
+};
+
+
+export const POST = async (req, res) => {
+  try {
+    connectToDb();
+    const payload = await req.json();
+    console.log(payload);
+    if (payload && payload.input &&  payload.input.length > 0) {
+      const newTeam = await Team.insertMany(payload.input)
+      return NextResponse.json({ error: false, data: newTeam });
+    } else {
+      return NextResponse.json({
+        error: true,
+        err: "Problem with body",
+        message: "Please fill in all the fields"
+      });
+    }
+  }
+  catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      error: true,
+      err: err,
+      message: "An unexpected error occurred please try again"
+    });
+  }
+};
