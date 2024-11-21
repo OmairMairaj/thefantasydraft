39d5f33ddda2e227e377d90d91c7339354ec97d2
haris316 <gogeto931@gmail.com>
2024-11-21 22:22:39 +0500

    Invite Code and Emails

--- app/api/fantasyleague/route.js
+++ app/api/fantasyleague/route.js
@@ -1,6 +1,8 @@
 import { FantasyLeague, FantasyTeam } from "@/lib/models";
 import { connectToDb } from "@/lib/utils";
 import { NextResponse } from "next/server";
+import { generateInviteCode } from "../../../lib/helpers";
+import { sendMultipleEmails } from "../../../lib/mail";
 
 export const GET = async (req, res) => {
   try {
@@ -31,14 +33,16 @@ export const POST = async (req, res) => {
         userID: payload.teams[0].userID,
         user_email: payload.teams[0].user_email,
       }
-      console.log("teamObj")
-      console.log(teamObj)
       userTeam = await FantasyTeam.create(teamObj);
-      console.log(userTeam)
     }
     payload.teams[0].team = userTeam._id;
+    payload.invite_code = await generateInviteCode();
     const newFantasyLeague = await FantasyLeague.create(payload);
-    return NextResponse.json({ error: false, leagueData: newFantasyLeague, teamData: userTeam });
+    if (newFantasyLeague.invite_code) {
+      const email_body = `<body><h3>Hello there!</h3><br /><p> You have been invited to Play Draft Fantasy! Please follow the link below to join ${newFantasyLeague.league_name}.</p><br /><p> Please accept your invite by following this link :</p><br /><br /><a href="${process.env.NEXT_PUBLIC_FRONTEND_URL}join-league-process?code=${newFantasyLeague.invite_code}">VIEW INVITE</a><br /><br /><p>If you have questions or you did not initiate this request, we are here to help. Email us at ${process.env.NEXT_PUBLIC_SUPPORT_EMAIL}</p><br /><br /><p>Regards,</p><p>Team Fantasy Draft</p></body>`
+      const emails = await sendMultipleEmails(newFantasyLeague.users_invited, "Fantasy League Invitation", email_body);
+      return NextResponse.json({ error: false, leagueData: newFantasyLeague, teamData: userTeam, emailData: emails });
+    }
   } catch (err) {
     return NextResponse.json({
       error: true,
