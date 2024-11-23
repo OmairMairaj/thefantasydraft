bda5d4112d211b3d808a56455394c8ea0bad96ae
haris316 <gogeto931@gmail.com>
2024-11-23 05:11:20 +0500

    League Joining Process

--- app/api/fantasyleague/code/route.js
+++ app/api/fantasyleague/code/route.js
@@ -1,19 +1,22 @@
-import { FantasyLeague, FantasyTeam } from "@/lib/models";
+import { FantasyLeague } from "@/lib/models";
 import { connectToDb } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async (req, res) => {
+  const url = new URL(req.url)
+  const reqCode = url.searchParams.get("code");
+  const reqEmail = url.searchParams.get("email");
   try {
     await connectToDb();
-    const url = new URL(req.url)
-    const reqCode = url.searchParams.get("code");
-    const reqEmail = url.searchParams.get("email");
     const data = await FantasyLeague.find({ invite_code: reqCode });
     if (data.length > 0) {
       if (data[0].users_onboard.indexOf(reqEmail) !== -1) return NextResponse.json({ error: true, message: "You are already a member of this league" });
       else if (data[0].users_invited.indexOf(reqEmail) !== -1) return NextResponse.json({ error: false, data: data[0] });
       else return NextResponse.json({ error: true, message: "You do not have a valid invite to this league. Please ask admin to invite you again" });
-    } else return NextResponse.json({ error: true, message: "No league found with this invite code" });
+    }
+    else return NextResponse.json({ error: true, message: "No league found with this invite code" });
   } catch (err) {
     console.log(err);
     return NextResponse.json({
