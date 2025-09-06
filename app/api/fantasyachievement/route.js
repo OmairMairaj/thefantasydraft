112cfae097a35b769b8a9f4e1a11ded22388b288
haris316 <gogeto931@gmail.com>
2025-09-06 05:53:03 +0500

    fantasy ach

--- app/api/fantasyachievement/route.js
+++ app/api/fantasyachievement/route.js
@@ -3,6 +3,14 @@ import { connectToDb } from "@/lib/utils";
 import { NextResponse } from "next/server";
 import mongoose from "mongoose";
 
+const ach_array = [
+  // {
+  //   name: "THE SNAKE",
+  //   desc: "Have a player from each position score 10+ points in a single week",
+  //   image_path: "/images/achievements/snake.png"
+  // }
+]
+
 export const GET = async (req, res) => {
   try {
     await connectToDb();
@@ -33,3 +41,23 @@ export const POST = async (req, res) => {
     });
   }
 };
+
+
+// export const POST = async (req, res) => {
+//   try {
+//     await connectToDb();    
+//     for (const ach of ach_array) {
+//       // console.log(ach.name);
+//       const newFantasyAchievement = await FantasyAchievement.create(ach);
+//       console.log(newFantasyAchievement);
+//     }
+//     return NextResponse.json({ error: false });
+//   } catch (err) {
+//     console.log(err);
+//     return NextResponse.json({
+//       error: true,
+//       err: err,
+//       message: "An unexpected error occurred, please try again."
+//     });
+//   }
+// };
