d2024aaa1fd701f220ef60d224eed2cc23431b1e
OmairMairaj <omairmairaj@gmail.com>
2025-10-06 21:37:15 +0500

    icon updates

--- app/layout.js
+++ app/layout.js
@@ -11,9 +11,11 @@ export const metadata = {
       { url: '/favicon.ico' },
       { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
-    apple: [{ url: '/apple-touch-icon.png' }],
-    shortcut: ['/favicon.ico'],
+    apple: [{ url: '/apple-touch-icon.png', sizes: '512x512', type: 'image/png' }],
+    shortcut: ['/apple-touch-icon.png'],
   },
   manifest: '/site.webmanifest', // optional PWA manifest (put this file in /public)
 };
